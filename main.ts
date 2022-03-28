1feb186c243acb9c01ad4b7c19af4fc534eb9f38
vojteaka7 <69011938+vojteaka7@users.noreply.github.com>
2022-03-28 15:54:20 +0200

    first commit, nefunguje

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
 basic.forever(function () {
-	
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) < 50) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) < 50) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    }
 })
